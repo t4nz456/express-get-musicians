f3c083c7debfe5f0d2fad401fd71ec59d0390f31
t4nz456 <t4nz1234@gmail.com>
2023-05-12 02:13:21 +0100

    part2

--- index.test.js
+++ index.test.js
@@ -10,13 +10,56 @@ const app = require('./src/app');
 const seedMusician = require("./seedData");
 
 
-describe('./musicians endpoint', () => {
-    // Write your tests here
-    
-    
+describe('musicians endpoint', () => {
+    test('It should respond with an array of musicians and status code 200', async () => {
+      const response = await request(app).get('/musicians');
+      expect(response.statusCode).toBe(200);
+      const musicians = JSON.parse(response.text);
+      expect(Array.isArray(musicians)).toBe(true);
+      expect(musicians.length).toBeGreaterThan(0);
+    });
+  
+});
 
+describe("Musicians Endpoints", () => {
+  let musicianId;
 
+  it("should create a new musician", async () => {
+    const res = await request(app)
+      .post("/musicians")
+      .send({
+        name: "John Doe",
+        instrument: "Guitar",
+        genre: "Rock"
+      });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.name).toEqual("John Doe");
+    expect(res.body.instrument).toEqual("Guitar");
+    expect(res.body.genre).toEqual("Rock");
+    musicianId = res.body.id;
+  });
 
+  it("should return all musicians", async () => {
+    const res = await request(app).get("/musicians");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.length).toBeGreaterThan(0);
+  });
 
-    
-})
+  it("should update an existing musician", async () => {
+    const res = await request(app)
+      .put(`/musicians/${musicianId}`)
+      .send({
+        name: "Jane Doe",
+        instrument: "Piano",
+        genre: "Jazz"
+      });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body[0]).toEqual(1);
+  });
+
+  it("should delete an existing musician", async () => {
+    const res = await request(app).delete(`/musicians/${musicianId}`);
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual(1); 
+  });
+});
--- src/app.js
+++ src/app.js
@@ -7,6 +7,30 @@ const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians 
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/musicians", async (req, res) => {
+    const musician = await Musician.findAll();
+    res.json(musician);
+})
+
+app.post("/musicians", async (req, res) => {
+    const data =  await Musician.create(req.body);
+    res.json(data);
+})
+
+app.put("/musicians/:id", async (req, res) => {
+    const find = await Musician.update(req.body, {where: {id: req.params.id}});
+    res.json(find);
+})
+
+app.delete("/musicians/:id", async (req, res) => {
+    const data = await Musician.destroy({where: {id: req.params.id}});
+    res.json(data);
+})
+
+
 
 
 
